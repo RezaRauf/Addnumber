a9e233b1a2746dc53255478b7297fd4b87ebe631
RezaRauf <89615965+RezaRauf@users.noreply.github.com>
2021-08-27 04:10:48 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+namespace addnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int First_Number, Second_Number, Total;
+
+            WriteLine("Please enter your first number:");
+            First_Number = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter your second number:");
+            Second_Number = Convert.ToInt32(ReadLine());
+
+            Total = (First_Number + Second_Number);
+            WriteLine("Your total is {0}", Total);
+
+
+
+
+        }
+    }
+}
